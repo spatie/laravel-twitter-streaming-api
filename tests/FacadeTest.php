d8869337df7e96c69f277b046dc0b91bcdc7517a
Freek Van der Herten <freek@spatie.be>
2026-02-21 22:49:59 +0100

    Add Laravel 13 support (#57)
    * Bump dependencies for Laravel 13
    
    * Update GitHub Actions for Laravel 13
    
    * Add PHP 8.5 support, fix coverage warnings, ensure Pest 4/PHPUnit 12
    
    * Clean up CI workflow for L13 support
    
    * Fix CI workflow GitHub Actions syntax
    
    * Convert @test to #[Test] for PHPUnit 12
    
    * Fix styling
    
    ---------
    
    Co-authored-by: Shift <shift@laravelshift.com>
    Co-authored-by: freekmurze <freekmurze@users.noreply.github.com>

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -2,19 +2,20 @@
 
 namespace Spatie\LaravelTwitterStreamingApi\Test;
 
+use PHPUnit\Framework\Attributes\Test;
 use Spatie\TwitterStreamingApi\PublicStream;
 use Spatie\TwitterStreamingApi\UserStream;
 use TwitterStreamingApi;
 
 class FacadeTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_can_return_a_public_stream()
     {
         $this->assertInstanceOf(PublicStream::class, TwitterStreamingApi::publicStream());
     }
 
-    /** @test */
+    #[Test]
     public function it_can_return_a_user_stream()
     {
         $this->assertInstanceOf(UserStream::class, TwitterStreamingApi::userStream());
